1db7cbbf95a7b1fba747e95fd8bbe931c13c39de
nuid32 <lvkuzvesov@proton.me>
2023-10-20 19:32:02 +0500

    fix: Add header guard for vga_print.h

--- kernel/vga_print.h
+++ kernel/vga_print.h
@@ -1,3 +1,6 @@
+#ifndef VGA_PRINT_H
+#define VGA_PRINT_H
+
 #include "types.h"
 
 void vga_terminal_initialize();
@@ -5,3 +8,5 @@ void vga_terminal_initialize();
 void vga_print(const char* s);
 
 void vga_set_color(u8 color);
+
+#endif
