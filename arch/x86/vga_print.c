351235bc8e4494849ffbd76bd752039b5f892dbd
nuid64 <lvkuzvesov@proton.me>
2024-01-01 10:30:26 +0500

    fix: Little improvements on vga_print

--- arch/x86/vga_print.c
+++ arch/x86/vga_print.c
@@ -1,26 +1,5 @@
 #include <kernel/types.h>
-
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+#include <kernel/vga_print.h>
  
 size_t vga_row;
 size_t vga_column;
@@ -59,9 +38,15 @@ void vga_terminal_initialize()
     }
 }
  
-void vga_setcolor(u8 color)
+void vga_set_color(u8 color)
 {
     vga_color = color;
+}
+
+void vga_set_pos(u8 x, u8 y)
+{
+    vga_column = x;
+    vga_row = y;
 }
  
 void vga_putentryat(char c, u8 color, size_t x, size_t y)
@@ -81,7 +66,7 @@ void scroll(u8 by)
         }
 }
  
-void putchar(char c)
+void vga_putchar(char c)
 {
     if (c == '\n') {
         vga_column = 0;
@@ -103,7 +88,7 @@ void putchar(char c)
 void vga_write(const char* s, size_t size)
 {
     for (size_t i = 0; i < size; i++)
-        putchar(s[i]);
+        vga_putchar(s[i]);
 }
  
 void vga_print(const char* s)
--- include/kernel/vga_print.h
+++ include/kernel/vga_print.h
@@ -2,9 +2,31 @@
 
 #include <kernel/types.h>
 
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+enum vga_color {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
+};
+ 
 void vga_terminal_initialize();
-
 void vga_set_color(u8 color);
-
+void vga_set_pos(u8 x, u8 y);
+void vga_putchar(char c);
 void vga_print(const char* s);
 void vga_print_num(u64 num);
