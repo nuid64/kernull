a3ab71d9f022cdfaa12a7a03177da85a38d39a02
nuid64 <lvkuzvesov@proton.me>
2024-02-17 21:56:00 +0500

    refactor: Minor changes in ps2_controller driver

--- include/kernel/ps2_controller.h
+++ include/kernel/ps2_controller.h
@@ -1,13 +1,14 @@
 #pragma once
+
 #include <kernel/types.h>
 
 /* INFO: Interface between keyboard/mice drivers and specific PS/2 controller */
 
-const u8 DATA_PORT   = 0x60;
-const u8 COMMAND_REG = 0x64; // Write-only
-const u8 STATUS_REG  = 0x64; // Read-only
+#define DATA_PORT    0x60
+#define COMMAND_REG  0x64 // Write-only
+#define STATUS_REG   0x64 // Read-only
 
-const u8 KBD_IRQ     = 0x21;
+#define KBD_IRQ      0x21
 
 void ps2_port1_write(u8 data);
 void ps2_port2_write(u8 data);
