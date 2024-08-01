ffc1cd586c0bd11fb6996e72221156c6f8ae3c4c
nuid64 <lvkuzvesov@proton.me>
2024-08-01 22:06:40 +0500

    misc: Change string.h declarations' alignment

--- include/kernel/string.h
+++ include/kernel/string.h
@@ -3,44 +3,44 @@
 #include <stddef.h>
 
 /* Search in S for C */
-void  *memchr(const void *s, int c, size_t n);
+void *memchr(const void *s, int c, size_t n);
 
 /* Compare N bytes of S1 and S2 */
-int    memcmp(const void *s1, const void *s2, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
 /* Copy N bytes of SRC to DEST */
-void  *memcpy(void *restrict dest, const void *src, size_t n);
+void *memcpy(void *restrict dest, const void *src, size_t n);
 
 /* Copy N bytes of SRC to DEST, guaranteeing
    correct behavior for overlapping strings */
-void  *memmove(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 
 /* Set N bytes of S to C */
-void  *memset(void *s, int c, size_t n);
+void *memset(void *s, int c, size_t n);
 
 /* Append SRC onto DEST */
-char    *strcat(char *restrict dest, const char *restrict src);
-char    *strncat(char *restrict dest, const char *restrict src, size_t n);
+char *strcat(char *restrict dest, const char *restrict src);
 /* Append no more than N characters from SRC onto DEST */
+char *strncat(char *restrict dest, const char *restrict src, size_t n);
 
 /* Search in S for C until terminating zero */
-char    *strchr(const char *s, int c);
+char *strchr(const char *s, int c);
 
 /* Compare S1 and S2 */
-int    strcmp(const char *s1, const char *s2);
+int strcmp(const char *s1, const char *s2);
 /* Compare N characters of S1 and S2 */
-int    strncmp(const char *s1, const char *s2, size_t n);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 /* Copy SRC to DEST */
-char    *strcpy(char *restrict dest, const char *restrict src);
+char *strcpy(char *restrict dest, const char *restrict src);
 /* Copy no more than N characters of SRC to DEST */
-char    *strncpy(char *restrict dest, const char *restrict src, size_t n);
+char *strncpy(char *restrict dest, const char *restrict src, size_t n);
 
 /* Return the length of S */
 size_t strlen(const char *s);
 
 /* Find the last occurrence of C in S */
-char    *strrchr(const char *s, int c);
+char *strrchr(const char *s, int c);
 
 /* Find the first substring */
-char    *strstr(const char *dom, const char *sub);
+char *strstr(const char *dom, const char *sub);
