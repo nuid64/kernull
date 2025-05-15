97511d7f6451cce98e6f218fc85cbdded141b3ab
nuid <lvkuzvesov@proton.me>
2025-05-15 17:21:10 +0500

    x86: Implement panic function for kernel error handling

---
+++ arch/x86/panic.c
@@ -0,0 +1,10 @@
+#include <arch/x86/vga_print.h>
+#include <kernel/panic.h>
+
+void panic(const char *msg)
+{
+	vga_print("KERNEL PANIC: ");
+	vga_print(msg);
+	for (;;)
+		__asm__ __volatile__("hlt");
+}
