f726a603a884298e25df4b47718a3f75c1c25679
nuid <lvkuzvesov@proton.me>
2025-05-15 16:32:17 +0500

    x86/asm: Add x86 register structure definition

---
+++ include/arch/x86/regs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+struct regs {
+    uint32_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, useresp, ss;
+};
