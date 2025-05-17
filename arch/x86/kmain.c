4d5e541ccf1fd511dc34f09b9b9cd55b0e4abce3
nuid <lvkuzvesov@proton.me>
2025-05-17 18:21:57 +0500

    x86/timer: Add PIT functions

--- arch/x86/kmain.c
+++ arch/x86/kmain.c
@@ -6,12 +6,14 @@
 
 extern void gdt_init(void);
 extern void idt_init(void);
+extern void pit_init(void);
 
 void kmain(void)
 {
 	vga_terminal_initialize();
 	gdt_init();
 	idt_init();
+	pit_init();
 	paging_init();
 
 	vga_set_color(VGA_COLOR_GREEN);
@@ -22,4 +24,6 @@ void kmain(void)
  / ,< /  __/ /  / / / / /_/ / / /  \n\
 /_/|_|\\___/_/  /_/ /_/\\__,_/_/_/   \n\
 ");
+	for (;;)
+		__asm__ __volatile__("hlt");
 }
---
+++ arch/x86/pit.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+#include <arch/x86/asm.h>
+#include <arch/x86/idt.h>
+#include <arch/x86/regs.h>
+
+#define PIT_A		0x40
+#define PIT_B		0x41
+#define PIT_C		0x42
+#define PIT_CONTROL 0x43
+
+#define PIT_SET	  0x36
+#define PIT_SCALE 1193180
+#define PIT_MASK  0xFF
+
+#define TIMER_IRQ 0x20
+
+static void pit_set_timer_freq(uint32_t hz)
+{
+	uint32_t divisor = PIT_SCALE / hz;
+	outb(PIT_CONTROL, PIT_SET);
+	outb(PIT_A, divisor & PIT_MASK);
+	outb(PIT_A, (divisor >> 8) & PIT_MASK);
+}
+
+uint32_t pit_interrupt(struct regs *)
+{
+	/* Do nothing for now */
+
+	return 0;
+}
+
+void pit_init()
+{
+	irq_set_handler(TIMER_IRQ, pit_interrupt);
+
+	pit_set_timer_freq(100);
+}
